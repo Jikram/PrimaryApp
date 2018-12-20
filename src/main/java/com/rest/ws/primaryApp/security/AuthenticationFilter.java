81bc745b3a18751a85a51c217bc04802131aadcb
Jikram <jamshaid.ikram@gmail.com>
2018-12-19 23:18:24 -0600

    Revert "Added authentication"
    This reverts commit 5e3400e

--- src/main/java/com/rest/ws/primaryApp/security/AuthenticationFilter.java
+++ src/main/java/com/rest/ws/primaryApp/security/AuthenticationFilter.java
@@ -1,29 +1,13 @@
 package com.rest.ws.primaryApp.security;
 
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rest.ws.primaryApp.model.requests.UserLoginRequestModel;
-
-import io.jsonwebtoken.Jwt;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -36,39 +20,6 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
-        try {
-
-            UserLoginRequestModel cred = new ObjectMapper()
-                    .readValue(request.getInputStream(), UserLoginRequestModel.class);
-
-                return authenticationManager
-                    .authenticate(new UsernamePasswordAuthenticationToken(
-                            cred.getEmail(),
-                            cred.getPassword(), new ArrayList<>()));
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication authResult) throws IOException, ServletException {
-
-        String username =((User) authResult.getPrincipal()).getUsername();
-        String token = Jwts.builder()
-                .setSubject(username)
-                .setExpiration(new Date(System.currentTimeMillis()+ SecurityConstants.EXPRIATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET)
-                .compact();
-
-        response.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
-
+        return super.attemptAuthentication(request, response);
     }
 }
-
-
-
-
